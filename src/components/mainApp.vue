d2fcaabc053c004ab607f024db796cd457f0f09b
StefanRaluca <raluca.bubulina@yahoo.com>
2024-04-09 17:16:26 +0200

    add section with actions and projects add svg in the company section

--- src/components/mainApp.vue
+++ src/components/mainApp.vue
@@ -152,10 +152,65 @@ export default {
         </div>
 
     </section>
-    <section class="the_company d-flex">
-
+    <section class="the_company d-flex position-relative">
+        <svg class="style_svg" xmlns="http://www.w3.org/2000/svg" width="150" height="100">
+            <defs>
+                <pattern width="150" height="100" id="a" patternContentUnits="userSpaceOnUse"
+                    patternUnits="userSpaceOnUse">
+                    <g stroke-width="0">
+                        <circle cx="5" cy="5" r="3" />
+                        <circle cx="20" cy="5" r="3" />
+                        <circle cx="35" cy="5" r="3" />
+                        <circle cx="50" cy="5" r="3" />
+                        <circle cx="65" cy="5" r="3" />
+                        <circle cx="80" cy="5" r="3" />
+                        <circle cx="95" cy="5" r="3" />
+                        <circle cx="110" cy="5" r="3" />
+                        <circle cx="125" cy="5" r="3" />
+                        <circle cx="140" cy="5" r="3" />
+                    </g>
+                    <g stroke-width="0">
+                        <circle cx="5" cy="20" r="3" />
+                        <circle cx="20" cy="20" r="3" />
+                        <circle cx="35" cy="20" r="3" />
+                        <circle cx="50" cy="20" r="3" />
+                        <circle cx="65" cy="20" r="3" />
+                        <circle cx="80" cy="20" r="3" />
+                        <circle cx="95" cy="20" r="3" />
+                        <circle cx="110" cy="20" r="3" />
+                        <circle cx="125" cy="20" r="3" />
+                        <circle cx="140" cy="20" r="3" />
+                    </g>
+                    <g stroke-width="0">
+                        <circle cx="5" cy="35" r="3" />
+                        <circle cx="20" cy="35" r="3" />
+                        <circle cx="35" cy="35" r="3" />
+                        <circle cx="50" cy="35" r="3" />
+                        <circle cx="65" cy="35" r="3" />
+                        <circle cx="80" cy="35" r="3" />
+                        <circle cx="95" cy="35" r="3" />
+                        <circle cx="110" cy="35" r="3" />
+                        <circle cx="125" cy="35" r="3" />
+                        <circle cx="140" cy="35" r="3" />
+                    </g>
+                    <g stroke-width="0">
+                        <circle cx="5" cy="50" r="3" />
+                        <circle cx="20" cy="50" r="3" />
+                        <circle cx="35" cy="50" r="3" />
+                        <circle cx="50" cy="50" r="3" />
+                        <circle cx="65" cy="50" r="3" />
+                        <circle cx="80" cy="50" r="3" />
+                        <circle cx="95" cy="50" r="3" />
+                        <circle cx="110" cy="50" r="3" />
+                        <circle cx="125" cy="50" r="3" />
+                        <circle cx="140" cy="50" r="3" />
+                    </g>
+                </pattern>
+            </defs>
+            <path fill="url(#a)" d="M0 0h150v100H0z" />
+        </svg>
         <div class="left_company">
-            <div class="container" style="width: 60%; margin-right: 30px; margin-top: 110px;">
+            <div class="container" style=" width: 60%; margin-right: 10px; margin-top: 90px;">
                 <h6 class="fw-semibold">ABOUT THE NETWORK</h6>
 
                 <h2> <span> The</span> Company</h2>
@@ -190,8 +245,143 @@ export default {
         </div>
         <div class="right_company">
             <img src="/img/about-4.jpg" alt="">
+
+        </div>
+        <svg class="style_svg_bottom" xmlns="http://www.w3.org/2000/svg" width="150" height="100">
+            <defs>
+                <pattern width="150" height="100" id="a" patternContentUnits="userSpaceOnUse"
+                    patternUnits="userSpaceOnUse">
+                    <g stroke-width="0">
+                        <circle cx="5" cy="5" r="3" />
+                        <circle cx="20" cy="5" r="3" />
+                        <circle cx="35" cy="5" r="3" />
+                        <circle cx="50" cy="5" r="3" />
+                        <circle cx="65" cy="5" r="3" />
+                        <circle cx="80" cy="5" r="3" />
+                        <circle cx="95" cy="5" r="3" />
+                        <circle cx="110" cy="5" r="3" />
+                        <circle cx="125" cy="5" r="3" />
+                        <circle cx="140" cy="5" r="3" />
+                    </g>
+                    <g stroke-width="0">
+                        <circle cx="5" cy="20" r="3" />
+                        <circle cx="20" cy="20" r="3" />
+                        <circle cx="35" cy="20" r="3" />
+                        <circle cx="50" cy="20" r="3" />
+                        <circle cx="65" cy="20" r="3" />
+                        <circle cx="80" cy="20" r="3" />
+                        <circle cx="95" cy="20" r="3" />
+                        <circle cx="110" cy="20" r="3" />
+                        <circle cx="125" cy="20" r="3" />
+                        <circle cx="140" cy="20" r="3" />
+                    </g>
+                    <g stroke-width="0">
+                        <circle cx="5" cy="35" r="3" />
+                        <circle cx="20" cy="35" r="3" />
+                        <circle cx="35" cy="35" r="3" />
+                        <circle cx="50" cy="35" r="3" />
+                        <circle cx="65" cy="35" r="3" />
+                        <circle cx="80" cy="35" r="3" />
+                        <circle cx="95" cy="35" r="3" />
+                        <circle cx="110" cy="35" r="3" />
+                        <circle cx="125" cy="35" r="3" />
+                        <circle cx="140" cy="35" r="3" />
+                    </g>
+                    <g stroke-width="0">
+                        <circle cx="5" cy="50" r="3" />
+                        <circle cx="20" cy="50" r="3" />
+                        <circle cx="35" cy="50" r="3" />
+                        <circle cx="50" cy="50" r="3" />
+                        <circle cx="65" cy="50" r="3" />
+                        <circle cx="80" cy="50" r="3" />
+                        <circle cx="95" cy="50" r="3" />
+                        <circle cx="110" cy="50" r="3" />
+                        <circle cx="125" cy="50" r="3" />
+                        <circle cx="140" cy="50" r="3" />
+                    </g>
+                </pattern>
+            </defs>
+            <path fill="url(#a)" d="M0 0h150v100H0z" />
+        </svg>
+    </section>
+    <section>
+        <div class="container text-center p-5" style="width: 70%;">
+            <div class="row justify-content-center">
+                <div class="col">
+
+                    <h6 class="fw-semibold">WE DO MORE FOR EVERYONE</h6>
+                    <h2>Actions & <span>Projects</span> </h2>
+                    <nav class="navbar navbar-expand-lg ms-5">
+                        <div class="container-fluid">
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav nav_projects">
+                                    <li class="nav-item">
+                                        <a class="nav-link nav_link_all" href="#">ALL</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">INSTITUTIONAL
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">SOCIAL
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">EVENTS
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">INNOVATION
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">ENVIRONMENT
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">TECHNOLOGY
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
+                </div>
+            </div>
+            <div class="row justify-content-center g-3 m-2 text-center">
+                <div class="col poster position-relative">
+                    <img src="/img/project-1.jpg" alt="">
+                    <h4 class="text-overlay">Academic professional program in social media</h4>
+                </div>
+                <div class="col poster  position-relative">
+                    <img src="/img/project-2.jpg" alt="">
+                    <h4 class="text-overlay">President's speech at the annual meeting</h4>
+                </div>
+                <div class="col poster  position-relative">
+                    <img src="/img/project-3.jpg" alt="">
+                    <h4 class="text-overlay">International business trip in Shanghai</h4>
+                </div>
+            </div>
+
+            <div class="row justify-content-center g-3 m-2 ">
+                <div class="col poster  position-relative">
+                    <img src="/img/project-4.jpg" alt="">
+                    <h4 class="text-overlay">Technology workshop with education theme</h4>
+                </div>
+                <div class="col poster  position-relative">
+                    <img src="/img/project-5.jpg" alt="">
+                    <h4 class="text-overlay">Donation of clothes and food to the partner NGO</h4>
+                </div>
+                <div class="col poster  position-relative">
+                    <img src="/img/project-6.jpg" alt="">
+                    <h4 class="text-overlay">Confraternization of the procurement team</h4>
+                </div>
+            </div>
         </div>
     </section>
+
+
 </template>
 
 
@@ -353,8 +543,35 @@ export default {
     }
 }
 
+.nav_projects {
+    gap: 10px !important;
+    /*     font-size: 15px !important; */
+}
+
+.nav_link_all {
+    background-color: var(--bool--text--decoration) !important;
+    ;
+}
 
 
+.col {
+    h2 {
+        font-weight: 800;
+        font-size: 40px;
+
+        span {
+            background-color: var(--bool--text--decoration);
+            color: var(--bool--buttons--principal);
+
+        }
+    }
+
+    h6 {
+        font-size: 12px;
+        color: var(--bool--buttons--principal);
+        margin-bottom: 20px;
+    }
+}
 
 .right_company {
     width: 50%;
@@ -362,5 +579,41 @@ export default {
     img {
         width: 100%;
     }
+}
+
+.poster {
+    width: 20%;
+
+    img {
+        width: 100%;
+        filter: brightness(45%);
+        border-radius: 10px;
+    }
+}
+
+.text-overlay {
+    position: absolute;
+    bottom: 15px;
+    color: white;
+    font-weight: 600;
+
+}
+
+.style_svg {
+    filter: invert(92%) sepia(17%) saturate(326%) hue-rotate(127deg) brightness(97%) contrast(90%);
+    position: absolute;
+    top: 4%;
+    left: 55%;
+    transform: translate(-50%, -50%);
+
+
+}
+
+.style_svg_bottom {
+    filter: invert(92%) sepia(17%) saturate(326%) hue-rotate(127deg) brightness(97%) contrast(90%);
+    position: absolute;
+    bottom: -18%;
+    left: 55%;
+    transform: translate(-50%, -50%);
 }
 </style>
